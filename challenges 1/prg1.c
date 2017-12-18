ef5cb59ed3c0c6323c9bbda3995ce09119ad7816
SARANYA <34641380+15EUEE128@users.noreply.github.com>
2017-12-19 00:50:35 +0530

    Update prg1.c

--- challenges 1/prg1.c
+++ challenges 1/prg1.c
@@ -4,12 +4,16 @@ int main(void)
 	int n;
 	printf("Enter the number");
 	scanf("%d",&n);
-	if(n>0)
+	if(n==0)
 	{
-		printf("53");
+		printf("\nZero");
+	}
+	else if(n>0)
+	{
+		printf("\nPositive");
 	}
 	else
 	{
-		printf("0");
+		printf("\nNegative");
 	}
 }
