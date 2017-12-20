432e80f7676fd6553289b8d1b965165e537fdb2e
SARANYA <34641380+15EUEE128@users.noreply.github.com>
2017-12-20 23:11:39 +0530

    Create prgm1.c

---
+++ challenges 1/set 2/prgm1.c
@@ -0,0 +1,12 @@
+//calculate the power of number
+#include <stdio.h>
+#include<math.h>
+int main(void)
+{
+	int x,y,n;
+	scanf("%d",&x);
+	scanf("%d",&n);
+	y=pow(x,n);
+	printf("%d",y);
+	return 0;
+}
