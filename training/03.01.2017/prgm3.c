72905147520277a764cf339c404a7f562a900cf3
SARANYA <34641380+15EUEE128@users.noreply.github.com>
2018-01-04 22:58:28 +0530

    Update prgm3.c

--- training/03.01.2017/prgm3.c
+++ training/03.01.2017/prgm3.c
@@ -1,19 +1,38 @@
-//to find different element in an array
-#include<stdio.h>
+// c program to find the different element in an array
+#include <stdio.h>
 int main(void)
 {
-	int a[100],n,i,max;
+	int n,i,a[100],odd=0,even=0;
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	
+	scanf("%d",&a[i]);
 	for(i=0;i<n;i++)
 	{
-		if(a[i]%2!=0)
+		if(a[i]%2==0)
 		{
-			printf("%d",a[i]);
+			even++;	
+		}
+		if(even>1)
+		{
+			if(a[i]%2==1)
+			{
+				printf("%d",a[i]);
+			}
+		}
+		
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]%2==1)
+		{
+			odd++;	
+		}
+		if(odd>1)
+		{
+			if(a[i]%2==0)
+			{
+				printf("%d",a[i]);
+			}
 		}
 		
 	}
