8658099b70ab5eeaae0d28ee880531fb8503544d
SARANYA <34641380+15EUEE128@users.noreply.github.com>
2018-01-13 19:55:36 +0530

    Update prgm5.c

--- challenges 1/set 5/prgm5.c
+++ challenges 1/set 5/prgm5.c
@@ -2,12 +2,12 @@
 int main(void)
 {
 	int n,count=0,rev;
-	scanf("%d",&n);
+	scanf("%ld",&n);
 	while(n!=0)
 	{
 		rev=n%10;
 		count=count+1;
 		n=n/10;
 	}
-	printf("%d",count);
+	printf("%ld",count);
 }
