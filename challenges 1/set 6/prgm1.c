45dd4a95ebf21725c132ebacbaf289ed364c75d4
SARANYA <34641380+15EUEE128@users.noreply.github.com>
2018-03-09 19:57:08 +0530

    Rename challenges 1/set 6 to challenges 1/set 6/prgm1.c

---
+++ challenges 1/set 6/prgm1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	int n,rem,rev=0;
+	scanf("%d",&n);
+	while(n>0)
+	{
+		rem=n%10;
+		rev=(rev*10)+rem;
+		n=n/10;
+	}
+	n=rev;
+	while(n>0)
+	{
+		rem=n%10;
+		printf("%d ",rem);
+		n=n/10;
+	}
+}
