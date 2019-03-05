413ce4d7095dd348b97db5179660ec2467872c34
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-05 19:15:06 +0530

    Create primenumberbetween.c

---
+++ primenumberbetween.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int low,high,i,flag;
+printf("enter the two number");
+scanf("%d%d",&high,&low);
+printf("enter the number between two numbers");
+while(low<high)
+{
+flag=0;
+for(i=0)
